9c6cce037a8fc5e09e07ad90c2134875c63a306d
Tran Duc Minh <tducminh@gmail.com>
2023-11-03 14:20:13 +0700

    Initial push 03/11/2023

---
+++ testPHKG/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace testPHKG;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ testPHKG/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace testPHKG;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
